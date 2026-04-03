e05e4ee966e1610c1a04bdefc9588ce5c7eaf9dd
RahulSannapureddy <rahul.sannapureddy@gmail.com>
2026-04-03 13:15:37 +0530

    chore: setup github actions deployment and update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Use your exact GitHub URL
+  site: 'https://RahulSannapureddy.github.io',
+  // For username.github.io repos, base must be '/'
+  base: '/', 
+});
